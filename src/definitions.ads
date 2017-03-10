2500b4a7109bee1cfe0171aafb70cc1f853e2aff
jrmarino <draco@marino.st>
2017-03-10 10:02:18 -0600

    Update copyright years
    Now we're pulling in synth code, so backdate copyright to 2015.

--- src/definitions.ads
+++ src/definitions.ads
@@ -7,7 +7,7 @@ package Definitions is
 
    raven_version_major : constant String := "0";
    raven_version_minor : constant String := "00";
-   copyright_years     : constant String := "2017";
+   copyright_years     : constant String := "2015-2017";
 
    raven_tool        : constant String := "ravenadm";
    variant_standard  : constant String := "standard";
@@ -20,8 +20,16 @@ package Definitions is
    broken_all        : constant String := "all";
    boolean_yes       : constant String := "yes";
    homepage_none     : constant String := "none";
+   jobs_per_cpu      : constant := 2;
 
    type supported_opsys is (dragonfly, freebsd, netbsd, openbsd, sunos, linux, macos);
    type supported_arch  is (x86_64, i386, aarch64);
+
+   type cpu_range is range 1 .. 32;
+   type scanners  is range cpu_range'First .. cpu_range'Last;
+   type builders  is range cpu_range'First .. cpu_range'Last * jobs_per_cpu;
+
+   -- Modify following with post-patch sed accordingly
+   platform_type : constant supported_opsys := dragonfly;
 
 end Definitions;
