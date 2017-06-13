e14240a0fdf973e12e3c5ef0a56566a5f2ae594b
jrmarino <draco@marino.st>
2017-06-13 11:05:00 -0500

    double port capacity

--- src/portscan.ads
+++ src/portscan.ads
@@ -78,7 +78,7 @@ private
 
    package CON renames Ada.Containers;
 
-   max_ports : constant := 2000;
+   max_ports : constant := 4000;
 
    type port_id is range -1 .. max_ports - 1;
    subtype port_index is port_id range 0 .. port_id'Last;
