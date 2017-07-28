e656106c511d05bd39ed5e3a8509cf40d8b9700e
jrmarino <draco@marino.st>
2017-07-28 16:04:45 -0500

    FIx incomplete rework of old portsmon link

--- www/progress.js
+++ www/progress.js
@@ -155,8 +155,8 @@ function skip_info (result, info) {
 }
 
 function originlink (bucket, origin) {
-	var parts = origin.split('/');
-	var ravenlink = '<a title="portsmon for "' + origin +
+	var parts = origin.split(':');
+	var ravenlink = '<a title="Port description of "' + origin +
 	'" href="http://ravenports.ironwolf.systems/bucket_' +
 	bucket + '/' + parts[0] + '/' + parts[1] + '">' + origin + '</a>';
 	return ravenlink;
