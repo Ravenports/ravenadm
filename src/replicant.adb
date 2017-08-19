dad5eb6004253d09e2ee70b3bf8f085defc4cfef
jrmarino <draco@marino.st>
2017-08-18 21:34:57 -0500

    Remove df filter on FreeBSD
    The use of "-t" argument on FreeBSD changes the output format of DF,
    breaking the cleanup routine.

--- src/replicant.adb
+++ src/replicant.adb
@@ -539,8 +539,8 @@ package body Replicant is
    function df_command return String is
    begin
       case platform_type is
+         when freebsd   => return "/bin/df -h";
          when dragonfly |
-              freebsd   |
               macos     |
               netbsd    |
               openbsd   => return "/bin/df -h -t null,tmpfs,devfs,procfs";
