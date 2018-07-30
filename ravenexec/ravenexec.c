0bbb0556a6ec7fc72015f94713beace4faa1b95d
jrmarino <draco@marino.st>
2018-07-30 18:23:19 -0500

    Revamp ravenexec, support killing process tree on linux
    For linux:
    Use the subreaper to reassign the parent ID of orphaned processes on
    linux.  The process tree kill is implemented by walking the /proc
    filesystem and checking the parent pid of every process, reaping those
    that match the top process.
    
    also:
    1) use fork instead of vfork (an attempt to fix mystery freeze during
    long builds on all platforms)
    2) move signal handlers after fork
    3) Add error message if execve fails
    4) refactor kill_process_tree()
    5) save top process pid in static variable to avoid calling it several
    times

--- ravenexec/ravenexec.c
+++ ravenexec/ravenexec.c
@@ -17,12 +17,20 @@
 #if defined __DragonFly__ || defined __FreeBSD__
 #include <sys/procctl.h>
 #endif
+#if defined __linux__
+#include <sys/prctl.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#endif
 #ifndef __unused
 #define __unused __attribute__((__unused__))
 #endif
 #ifndef W_EXITCODE
 #define W_EXITCODE(ret, sig)	((ret) << 8 | (sig))
 #endif
+
+static pid_t ravenexec;
 
 /*
  * reap_process kills per given pid and waits for it to return
@@ -43,73 +51,133 @@ int reap_process (pid_t dead_pid_walking)
 int
 kill_process_tree (pid_t reaper_pid)
 {
-#ifdef PROC_REAP_STATUS
-#  ifdef __FreeBSD__
+   if (reaper_pid <= 0) {
+        return (-1);
+   }
+#if defined(__FreeBSD__) && defined(PROC_REAP_ACQUIRE)
    struct procctl_reaper_status info;
    struct procctl_reaper_kill killemall;
 
-   if (reaper_pid > 0)
+   if (procctl(P_PID, reaper_pid, PROC_REAP_STATUS, &info) == 0)
    {
-      if (procctl(P_PID, reaper_pid, PROC_REAP_STATUS, &info) == 0)
+      if (info.rs_children != 0)
       {
-         if (info.rs_children != 0)
+         killemall.rk_sig = SIGKILL;
+         killemall.rk_flags = 0;
+         if (procctl(P_PID, reaper_pid, PROC_REAP_KILL, &killemall) != 0)
          {
-            killemall.rk_sig = SIGKILL;
-            killemall.rk_flags = 0;
-            if (procctl(P_PID, reaper_pid, PROC_REAP_KILL, &killemall) != 0)
-            {
-                return (-1);
-            }
+             return (-1);
          }
       }
-      return (0);
    }
-   return (-1);
-#  endif  /* __FreeBSD__ */
-#  ifdef __DragonFly__
+   return (0); 
+#elif defined(__DragonFly__) && defined(PROC_REAP_ACQUIRE)
    union reaper_info info;
    int keep_going = 1;
 
-   if (reaper_pid > 0)
+   while (keep_going)
    {
-      while (keep_going)
+      keep_going = 0;
+      if (procctl(P_PID, reaper_pid, PROC_REAP_STATUS, &info) == 0)
       {
-         keep_going = 0;
-         if (procctl(P_PID, reaper_pid, PROC_REAP_STATUS, &info) == 0)
+         if (info.status.pid_head > 0)
          {
-            if (info.status.pid_head > 0)
+            if (reap_process (info.status.pid_head) == 0)
             {
-               if (reap_process (info.status.pid_head) == 0)
+               keep_going = 1;
+            }
+            else
+            {
+               return (-1);
+            }
+         }
+      }
+   }
+   return (0);
+#elif defined(__linux__)
+   /*
+    * Iterate /proc pseudo-filesystem repeatedly until no
+    * process ID has the ravenexec process as its parent.
+    */
+   for (;;)
+   {
+      DIR *dir;
+      struct dirent* ent;
+      char *endptr;
+      char buffer[512];
+      int found_ppid = 0;
+
+      dir = opendir("/proc");
+      if (dir == NULL)
+      {
+         return (-1);
+      }
+      
+      while ((ent = readdir(dir)) != NULL)
+      {
+         long lpid;
+         char *s_ppid;
+         pid_t parent_id;
+         /*
+          * if endptr is not a null character, the directory is not
+          * numeric so ignore it
+          */
+         lpid = strtol(ent->d_name, &endptr, 10);
+         if (*endptr != '\0')
+         {
+            continue;
+         }
+         snprintf(buffer, sizeof(buffer), "/proc/%ld/stat", lpid);
+         FILE *fp = fopen(buffer, "r");
+         if (fp)
+         {
+            size_t size = fread(buffer, sizeof(char), sizeof(buffer), fp);
+            fclose (fp);
+            if (size > 0)
+            {
+               strtok(buffer, " ");             // (1) pid   %d
+               strtok(NULL, " ");               // (2) comm  %s
+               strtok(NULL, " ");               // (3) state %c
+               s_ppid = strtok(NULL, " ");      // (4) ppid  %d
+
+               parent_id = (pid_t) atoi(s_ppid);
+               if (parent_id == reaper_pid)
                {
-                  keep_going = 1;
-               }
-               else
-               {
-                  return (-1);
+                  found_ppid = 1;
+                  if (reap_process ((pid_t) lpid) == -1)
+                  {
+                     closedir (dir);
+                     return (-1);
+                  }
                }
             }
          }
       }
-      return (0);
+      closedir(dir);
+
+      if (!found_ppid) {
+         break;
+      }
    }
-   return (-1);
-#  endif  /* __DragonFly__ */
-#else
-/* process reaping is not supported on this system */
    return (0);
-#endif    /* PROC_REAP_STATUS */
+#else
+   /* process reaping is not supported on this system */
+   return (0);
+#endif  /* __FreeBSD__ || __DragonFly__ || __linux__ */
 }
 
 void
 handler (int signo __unused)
 {
-   kill_process_tree (getpid());
+   kill_process_tree (ravenexec);
    exit(1);
 }
 
 int
 main (int argc, char *argv[])
 {
+   ravenexec = getpid();
+
    if (argc < 4)
    {
       return (-2);
@@ -137,7 +205,16 @@ main (int argc, char *argv[])
     * Set current process as the reaper for itself and future children
     * Interface identical for FreeBSD and DragonFly
     */
-   if (procctl(P_PID, getpid(), PROC_REAP_ACQUIRE, NULL) < 0)
+   if (procctl(P_PID, ravenexec, PROC_REAP_ACQUIRE, NULL) < 0)
+   {
+      return (-5);
+   }
+#endif
+#ifdef PR_SET_CHILD_SUBREAPER
+   /*
+    * Linux: Set current process as reaper for future children
+    */
+   if (prctl(PR_SET_CHILD_SUBREAPER, 1) < 0)
    {
       return (-5);
    }
@@ -146,10 +223,7 @@ main (int argc, char *argv[])
    pid_t pid;
    int status = 0;
 
-   signal(SIGUSR1, handler);
-   signal(SIGINT,  handler);
-
-   pid = vfork();
+   pid = fork();
    if (pid < 0)
    {
       return (-6);
@@ -158,10 +232,20 @@ main (int argc, char *argv[])
    {
       /* child */
       execv (argv[2], (argv + 2));
+
+      /* command failed to execute */
+      dprintf (STDERR_FILENO, "Command execution failed: %s\n", argv[2]);
+      dprintf (STDERR_FILENO, "               arguments:");
+      for (int x = 2; x < argc; x++)
+         dprintf (STDERR_FILENO, " %s", argv[x]);
+      dprintf (STDERR_FILENO, "\n");
       _exit (1);
    }
 
    /* Parent */
+   signal(SIGUSR1, handler);
+   signal(SIGINT,  SIG_IGN);
+
    for (;;) {
       pid_t cpid;
 
@@ -174,6 +258,6 @@ main (int argc, char *argv[])
          break;
       }
    }
-   kill_process_tree (getpid());
+   kill_process_tree (ravenexec);
    return (WEXITSTATUS (status));
 }
