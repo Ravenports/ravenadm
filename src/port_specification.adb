109e0fac76f4120d6ead4b4f5bc662bf73c319e0
jrmarino <draco@marino.st>
2018-08-13 09:10:50 -0500

    fix false positive: Allow EXRUN to be "mysql" and "pgsql"

--- src/port_specification.adb
+++ src/port_specification.adb
@@ -1356,8 +1356,12 @@ package body Port_Specification is
                raise wrong_type with "subpackage key '" & key & "' has not been defined.";
             end if;
             if not valid_dependency_format (value) then
-               if value /= "ssl" and then value /= "python" and then value /= "tcl"
-                 and then value /= "perl"
+               if value /= "ssl" and then
+                 value /= "python" and then
+                 value /= "tcl" and then
+                 value /= "perl" and then
+                 value /= "mysql" and then
+                 value /= "pgsql"
                then
                   raise wrong_value with "invalid dependency format '" & value & "'";
                end if;
