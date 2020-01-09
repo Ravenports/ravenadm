08edc58bb3061af2c37867844944452021aa3c8b
jrmarino <draco@marino.st>
2020-01-09 13:28:36 -0600

    Storage error might be coming from get_file_contents()
    Try to handle this case (still working on ansible)

--- src/file_operations.adb
+++ src/file_operations.adb
@@ -17,45 +17,53 @@ package body File_Operations is
    --------------------------------------------------------------------------------------------
    function get_file_contents (dossier : String) return String
    is
-      File_Size : Natural := Natural (DIR.Size (dossier));
-
-      subtype File_String    is String (1 .. File_Size);
-      package File_String_IO is new Ada.Direct_IO (File_String);
-
-      file_handle : File_String_IO.File_Type;
-      contents    : File_String;
-      attempts    : Natural := 0;
+      File_Size : constant Natural := Natural (DIR.Size (dossier));
    begin
       if File_Size = 0 then
          return "";
       end if;
-      --  The introduction of variants causes a buildsheet to be scanned once per variant.
-      --  It's possible (even common) for simultaneous requests to scan the same buildsheet to
-      --  occur.  Thus, if the file is already open, wait and try again (up to 5 times)
-      loop
-         begin
-            File_String_IO.Open  (File => file_handle,
-                                  Mode => File_String_IO.In_File,
-                                  Name => dossier);
-            exit;
-         exception
-            when File_String_IO.Status_Error | File_String_IO.Use_Error =>
-               if attempts = 5 then
-                  raise file_handling with "get_file_contents: remained open: " & dossier;
-               end if;
-               attempts := attempts + 1;
-               delay 0.1;
-         end;
-      end loop;
-      File_String_IO.Read  (File => file_handle, Item => contents);
-      File_String_IO.Close (file_handle);
-      return contents;
+
+      declare
+         subtype File_String    is String (1 .. File_Size);
+         package File_String_IO is new Ada.Direct_IO (File_String);
+
+         file_handle : File_String_IO.File_Type;
+         contents    : File_String;
+         attempts    : Natural := 0;
+      begin
+
+         --  The introduction of variants causes a buildsheet to be scanned once per variant.
+         --  It's possible (even common) for simultaneous requests to scan the same buildsheet to
+         --  occur.  Thus, if the file is already open, wait and try again (up to 5 times)
+         loop
+            begin
+               File_String_IO.Open  (File => file_handle,
+                                     Mode => File_String_IO.In_File,
+                                     Name => dossier);
+               exit;
+            exception
+               when File_String_IO.Status_Error | File_String_IO.Use_Error =>
+                  if attempts = 5 then
+                     raise file_handling with "get_file_contents: remained open: " & dossier;
+                  end if;
+                  attempts := attempts + 1;
+                  delay 0.1;
+            end;
+         end loop;
+         File_String_IO.Read  (File => file_handle, Item => contents);
+         File_String_IO.Close (file_handle);
+         return contents;
+      exception
+         when others =>
+            if File_String_IO.Is_Open (file_handle) then
+               File_String_IO.Close (file_handle);
+            end if;
+            raise file_handling with "get_file_contents(" & dossier & ") failed";
+      end;
    exception
-      when others =>
-         if File_String_IO.Is_Open (file_handle) then
-            File_String_IO.Close (file_handle);
-         end if;
-         raise file_handling with "get_file_contents: " & dossier;
+      when Storage_Error =>
+         raise file_handling with "get_file_contents(" & dossier & ") failed to allocate memory";
+
    end get_file_contents;
 
 
