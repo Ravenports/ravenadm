e8d91eee7058d4d66c574aaca24662b1665ac367
jrmarino <draco@marino.st>
2020-03-22 19:31:11 -0500

    Oops, we already bumped version
    Bring it back to 1.42

--- src/definitions.ads
+++ src/definitions.ads
@@ -6,7 +6,7 @@ package Definitions is
    pragma Pure;
 
    raven_version_major : constant String := "1";
-   raven_version_minor : constant String := "43";
+   raven_version_minor : constant String := "42";
    copyright_years     : constant String := "2015-2020";
 
    raven_tool        : constant String := "ravenadm";
