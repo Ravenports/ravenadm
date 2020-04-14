29b4274c5c21006411f7ef76c3816add8c29b500
jrmarino <draco@marino.st>
2020-04-14 16:46:17 -0500

    Let's have cran pull in the entire R enviroment

--- src/port_specification-transform.adb
+++ src/port_specification-transform.adb
@@ -1060,7 +1060,7 @@ package body Port_Specification.Transform is
    procedure apply_cran_module (specs : in out Portspecs)
    is
       module     : String := "cran";
-      dependency : String := "R:primary:standard";
+      dependency : String := "R:complete:standard";
    begin
       if specs.uses_base.Contains (HT.SUS (module)) then
          add_buildrun_depends (specs, dependency);
