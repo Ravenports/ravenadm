4f0c8dc1652d4c50f960880fa14c7885f8d91396
jrmarino <draco@marino.st>
2020-05-02 20:43:25 -0500

    Don't use quotations with /usr/bin/env
    They are considered literatlly.  Try escaping spaces instead.

--- src/portscan-operations.adb
+++ src/portscan-operations.adb
@@ -517,7 +517,7 @@ package body PortScan.Operations is
       function nvpair (name : String; value : HT.Text) return String is
       begin
          return
-           name & LAT.Equals_Sign & LAT.Quotation & HT.USS (value) & LAT.Quotation & LAT.Space;
+           name & LAT.Equals_Sign & HT.replace_char (HT.USS (value), LAT.Space, "\ ");
       end nvpair;
       common_env : constant String :=
         nvpair ("PROFILE", PM.configuration.profile) &
