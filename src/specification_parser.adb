68bcf068ace54dad47d2c20f2d5704b07a9fc8cf
jrmarino <draco@marino.st>
2023-01-31 20:18:12 -0600

    fix typo

--- src/specification_parser.adb
+++ src/specification_parser.adb
@@ -1767,7 +1767,7 @@ package body Specification_Parser is
          return "check above errors to determine which option groups are too small";
       end if;
       if not spec.post_module_argument_check then
-         return "The argumens of a USES modules are not valid.";
+         return "The arguments of a USES modules are not valid.";
       end if;
       return "";
    end late_validity_check_error;
