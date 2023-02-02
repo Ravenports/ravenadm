f59f101f121c25629ad9d1c730df7f04e5055d82
jrmarino <draco@marino.st>
2023-02-01 18:22:52 -0600

    bump to version 1.95

--- src/definitions.ads
+++ src/definitions.ads
@@ -6,7 +6,7 @@ package Definitions is
    pragma Pure;
 
    raven_version_major : constant String := "1";
-   raven_version_minor : constant String := "94";
+   raven_version_minor : constant String := "95";
    copyright_years     : constant String := "2015-2023";
 
    raven_tool        : constant String := "ravenadm";
