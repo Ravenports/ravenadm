6315b94161faf57fec022bdc5746800727911876
jrmarino <draco@marino.st>
2024-06-26 11:10:40 -0500

    drop unused code
    Apparently the creation of a pkg repo configuration file was never actually used.
    I'm not sure how it could be -- too many assumptions to make.

--- src/repository.ads
+++ src/repository.ads
@@ -5,9 +5,6 @@ with Definitions; use Definitions;
 
 package Repository is
 
-   --  Create a pkg repo conf file (requires root permission)
-   function write_pkg_repos_configuration_file return Boolean;
-
    --  Rebuild the local repository with pkg(8)
    procedure rebuild_local_respository (remove_invalid_packages : Boolean);
 
@@ -46,9 +43,6 @@ private
 
    --  Returns octal failure of file permissions or "000" upon command failure
    function file_permissions (full_path : String) return String;
-
-   --  Query pkg(8)'s repos_dir configuration instead of assuming default
-   function get_repos_dir return String;
 
    --  This routine first removes all invalid packages (package from removed
    --  port or older version) and inserts the origins of the remaining packages
