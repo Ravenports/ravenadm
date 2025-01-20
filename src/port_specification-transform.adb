09c3573565f321a76a1533919e9b672dbe89d589
jrmarino <draco@marino.st>
2025-01-19 19:50:52 -0600

    Handle iconv differently with glibc systems (#87)
    This can be overridden with "standalone" argument

--- src/port_specification-transform.adb
+++ src/port_specification-transform.adb
@@ -1375,8 +1375,11 @@ package body Port_Specification.Transform is
    is
       module     : constant String := "iconv";
       dep_prefix : constant String := "libiconv";
+      glibc      : constant Boolean := DIR.Exists ("/usr/include/iconv.h");
    begin
-      generic_devlib_module (specs, module, dep_prefix);
+      if not glibc or else argument_present (specs, module, "standalone") then
+         generic_devlib_module (specs, module, dep_prefix);
+      end if;
    end apply_libiconv_module;
 
 
