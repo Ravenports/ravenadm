aa6590d542901d445887f9a63c21dd7aa05e51fa
jrmarino <draco@marino.st>
2025-11-10 13:59:23 -0600

    Ensure python_used is always defined
    Even if python is not defined in USED=

--- src/port_specification-transform.adb
+++ src/port_specification-transform.adb
@@ -1825,6 +1825,7 @@ package body Port_Specification.Transform is
 
    begin
       if not specs.uses_base.Contains (HT.SUS (module)) then
+         specs.used_python := HT.SUS (PYVARIANT);
          return;
       end if;
 
